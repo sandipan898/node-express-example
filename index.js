45a3a6e8d7735c2d070d86614ba9f85868320b08
sandipan898 <sandipan.das898@gmail.com>
2020-10-07 01:49:09 +0530

    updating dishRouter.js

--- index.js
+++ index.js
@@ -13,29 +13,6 @@ app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
 
-
-app.get('/dishes/:dishId', (req, res, next) => {
-    res.end('Will send details of the dish: '
-        + req.params.dishId + ' to you!');
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end("POST operation not supported on /dishes/" +
-        req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name +
-        'with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dishe: ' + req.params.dishId);
-});
-
-
 app.use(express.static(__dirname+'/public'))
 
 app.use((req, res, next) => {
